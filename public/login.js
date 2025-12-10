605f11736d0386377ac3e3b27291275f1e460f7b
sajavonas <73395447+sajavonas@users.noreply.github.com>
2025-12-10 20:30:02 +0200

    Delete public directory

--- public/login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const password = document.getElementById('password').value;
-    if(password === 'secret123') {
-        window.location.href = '/chat.html';
-    } else {
-        document.getElementById('error').innerText = 'Incorrect password!';
-    }
-});
