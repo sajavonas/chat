7d02a1eeb19e02f82c9e981074a6d3f0dcc4486f
sajavonas <73395447+sajavonas@users.noreply.github.com>
2025-12-22 04:33:06 +0200

    Delete public directory

--- public/config.js
+++
@@ -1,4 +0,0 @@
-
-// Вставьте URL проекта и ключ anon/publishable
-window.SUPABASE_URL = "https://sewvpuwbyeyqttikgpah.supabase.co";
-window.SUPABASE_ANON_KEY = "Ysb_publishable_vQnhaO60slNnO5rSk9FELQ_tpfRBTDo";
